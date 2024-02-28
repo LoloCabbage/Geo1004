5fe9b93bae43d2b741b0b20a852a3eaea34357b8
LoloCabbage <108572718+LoloCabbage@users.noreply.github.com>
2024-02-28 14:29:31 +0100

    TRIANGULATION FINISHED

--- hw01/main.cpp
+++ hw01/main.cpp
@@ -28,12 +28,23 @@ typedef CGAL::Triangulation_data_structure_2<VertexBase, FaceBaseWithInfo> Trian
 typedef CGAL::Constrained_Delaunay_triangulation_2<Kernel, TriangulationDataStructure, Tag> Triangulation;
 
 
-const std::string input_file = "/mnt/c/Users/Acer/Documents/GitHub/Geo1004/Geo1004/hw01/NL.IMBAG.Pand.0503100000020110-0.obj"; // the faculty building
+const std::string input_file = "/mnt/c/Users/Acer/Documents/GitHub/Geo1004/Geo1004/hw01/L_shape.obj"; // the faculty building
 const std::string output_file = "/mnt/c/Users/Acer/Documents/GitHub/Geo1004/Geo1004/hw01/faculty.obj";
 
 struct Vertex {
     double x, y, z;
+    int index;
 };
+
+struct Vertex2D {
+    int index;                    // the same index as the 3D vertex
+    Kernel::Point_2 point;     // coordinates of the 2D vertex
+};
+
+struct triangle {
+    int v1, v2, v3;
+};
+
 
 struct Face {
     std::list<int> boundary; // ids of vertices vector, boundary = []
@@ -56,14 +67,16 @@ int main(int argc, const char * argv[]) {
         std::string line;
 
         //// Parse line by line
+        int i = 1; // index of the vertex
         while (getline(input_stream, line)) {
-            std::cout << line << std::endl;
+            //std::cout << line << std::endl;
 
             std::istringstream line_stream(line);
             char line_type; // output the char type in order
             line_stream >> line_type;
 
             //// Vertex: store all the coordination into vertices
+
             if (line_type == 'v') {
                 vertices.emplace_back(); // append a new vertex
                 double x, y, z; // output the double type in order
@@ -71,6 +84,9 @@ int main(int argc, const char * argv[]) {
                 vertices.back().x = x; //.back() returns the last element of the vector = append
                 vertices.back().y = y;
                 vertices.back().z = z;
+                vertices.back().index = i;
+                std::cout << "Vertex" << i << ":(" << x << ", " << y << ", " << z << ")" << std::endl;
+                i++;
             }
 
             //// Face: store all the vertices into faces
@@ -91,20 +107,6 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
 
-////     Print vertices, start with index 0
-//  int i = 0;
-//  for (auto const &vertex: vertices) {
-//    std::cout << "Vertex " << i++ << ": " << "(" << vertex.x << ", " << vertex.y << ", " << vertex.z << ")" << std::endl;
-//  }
-//
-//////    Print faces, start with index 0
-//  i = 0;
-//  for (auto const &face: faces) {
-//    std::cout << "Face " << i++ << ": ";
-//    for (auto const &vertex: face.boundary) std::cout << " " << vertex;
-//    std::cout << std::endl;
-//  }
-
 ////     Best fitting planes (to do)
     for (auto &face: faces) { // go through each face
         std::vector<Kernel::Point_3> planes; // create a vector of points
@@ -118,34 +120,47 @@ int main(int argc, const char * argv[]) {
     }
 
 ////     Triangulate faces (to do)
+    std::vector<triangle> triangles;
     for (auto &face: faces) {
-        std::vector<Kernel::Point_2> projected_2d_boundary;
-        CGAL::Vector_3<Kernel> normal = face.best_plane.orthogonal_vector(); // normal vector of the plane
-        CGAL::Vector_3<Kernel> u = face.best_plane.base1(); // base vector u
-        CGAL::Vector_3<Kernel> v = face.best_plane.base2(); // base vector v
-        auto first_vertex_id = face.boundary.front(); // first vertex id
-        Kernel::Point_3 first_vertex(vertices[first_vertex_id].x, vertices[first_vertex_id].y, vertices[first_vertex_id].z); // first vertex
-        Kernel::Point_3 first_projected_vertex = face.best_plane.projection(first_vertex); // first projected vertex
-        CGAL::Vector_3<Kernel> first_projected_vector(first_projected_vertex, CGAL::ORIGIN);// first projected vector
-        std::cout << "2D plane info: " << u << "," << v << "," << first_projected_vertex << std::endl;
+        // create a vector of 2D points
+        std::vector<Vertex2D> projected_2d_boundary;
+        // get the normal vector of the plane
+        CGAL::Vector_3<Kernel> normal = face.best_plane.orthogonal_vector();
+        // get the base vector of the plane
+        CGAL::Vector_3<Kernel> u = face.best_plane.base1();
+        CGAL::Vector_3<Kernel> v = face.best_plane.base2();
+        // get the first vertex of the face as the reference point
+        auto first_vertex_id = face.boundary.front();
+        Kernel::Point_3 first_vertex(vertices[first_vertex_id].x, vertices[first_vertex_id].y, vertices[first_vertex_id].z);
+        Kernel::Point_3 first_projected_vertex = face.best_plane.projection(first_vertex);
+        CGAL::Vector_3<Kernel> first_projected_vector(first_projected_vertex, CGAL::ORIGIN);
+
+        // std::cout << "2D plane info: " << u << "," << v << "," << first_projected_vertex << std::endl;
+        // project all the vertices to the 2D plane
         for (auto &vertex_id: face.boundary) {
-            // use the first vertex as the origin
+            // project the points to 3D plane
             Kernel::Point_3 p(vertices[vertex_id].x, vertices[vertex_id].y, vertices[vertex_id].z);
+            int index = vertices[vertex_id].index;
             Kernel::Point_3 projected_p = face.best_plane.projection(p);
-            CGAL::Vector_3<Kernel> projected_vector(projected_p, CGAL::ORIGIN);
+            CGAL::Vector_3<Kernel> projected_vector(projected_p, CGAL::ORIGIN);//point to vector for calculation
             CGAL::Vector_3<Kernel> difference_vector = projected_vector - first_projected_vector;
             // project the 3D points to 2D
             CGAL::Vector_2<Kernel> projected_2d_vector(difference_vector*u, difference_vector*v);
-            Kernel ::Point_2 projected_2d_point(projected_2d_vector.x(), projected_2d_vector.y());
-            std::cout << "Projected 2D point: " << projected_2d_point << std::endl;
+            Vertex2D projected_2d_point;
+            projected_2d_point.index = index;
+            Kernel ::Point_2 to_2d_point(projected_2d_vector.x(), projected_2d_vector.y());
+            projected_2d_point.point = to_2d_point;
+            std::cout << "2D point: " << index << "," << to_2d_point << std::endl;
             projected_2d_boundary.push_back(projected_2d_point);
         }
         // constraint triangulation
         for (int i = 0; i < projected_2d_boundary.size(); ++i) {
             // insert constraint with consecutive vertices
-            face.triangulation.insert_constraint(projected_2d_boundary[i],
-                                                 projected_2d_boundary[(i+1)%projected_2d_boundary.size()]);
+            face.triangulation.insert_constraint(projected_2d_boundary[i].point,
+                                                 projected_2d_boundary[(i+1)%projected_2d_boundary.size()].point);
+            std::cout << "Constraint: " << projected_2d_boundary[i].point << " " << projected_2d_boundary[(i+1)%projected_2d_boundary.size()].point << std::endl;
         }
+
             // check the validity of the triangulation
         assert(face.triangulation.is_valid());
             // triangulate the polygon
@@ -162,18 +177,33 @@ int main(int argc, const char * argv[]) {
             }
 
         }
-        // output the vertex of triangles
+
+
+
         for (auto face_it = face.triangulation.finite_faces_begin();
              face_it != face.triangulation.finite_faces_end(); ++face_it) {
             if (face_it->info().interior) {
-                std::cout << "Triangle: ";
+                int array[3] = {0, 0, 0};
                 for (int i = 0; i < 3; ++i) {
                     auto vertex = face_it->vertex(i);
-                    std::cout << " " << vertex->point();
+                    // find the vertex in projected_2d_boundary
+                    for (auto const &vertex2d: projected_2d_boundary) {
+                        if (vertex2d.point == vertex->point()) { // find the vertex
+                            array[i] = vertex2d.index;
+                        }
+                    }
                 }
-                std::cout << std::endl;
+                triangle t{};
+                t.v1 = array[0];
+                t.v2 = array[1];
+                t.v3 = array[2];
+                triangles.push_back(t);
             }
         }
+    }
+    // output all triangles
+    for (auto const &t: triangles) {
+        std::cout << "f " << t.v1 << " " << t.v2 << " " << t.v3 << std::endl;
     }
 
 ////     Label triangulation (to do)
@@ -186,5 +216,9 @@ int main(int argc, const char * argv[]) {
     // output all original vertices
     for (auto const &vertex: vertices) {
         output_stream << "v " << vertex.x << " " << vertex.y << " " << vertex.z << std::endl;
+    }
+    // output all triangles
+    for (auto const &t: triangles) {
+        output_stream << "f " << t.v1 << " " << t.v2 << " " << t.v3 << std::endl;
     }
 }
